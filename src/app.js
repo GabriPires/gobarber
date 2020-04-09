59be9aecb59f7025c1a4dfa2f49ab695c17e89cc
GabriPires <gabriel.santos.pes@gmail.com>
2020-04-09 19:04:44 -0300

    Configurando Sentry para tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
 class App {
    constructor() {
       this.server = express();
+
+      Sentry.init(sentryConfig);
+
       this.middlewares();
       this.routes();
    }
 
    middlewares() {
+      this.server.use(Sentry.Handlers.requestHandler());
       this.server.use(express.json());
       this.server.use(
          '/files',
@@ -21,6 +27,7 @@ class App {
 
    routes() {
       this.server.use(routes);
+      this.server.use(Sentry.Handlers.errorHandler());
    }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+   dsn:
+      'https://66824bd51d904447a464b8973f19a8ba@o375712.ingest.sentry.io/5195612',
+};
