82d20805635b66c08753690618d9e386d98fd8ee
GabriPires <gabriel.santos.pes@gmail.com>
2020-04-09 17:14:02 -0300

    Configurando Nodemailer para envio de emails

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
    async index(req, res) {
       const { page = 1 } = req.query;
@@ -114,7 +116,13 @@ class AppointmentController {
    }
 
    async delete(req, res) {
-      const appointment = await Appointment.findByPk(req.params.id);
+      const appointment = await Appointment.findByPk(req.params.id, {
+         include: {
+            model: User,
+            as: 'provider',
+            attributes: ['name', 'email'],
+         },
+      });
 
       // Verificando se o id do usuário do agendamento é diferente do logado
       if (appointment.user_id !== req.userId) {
@@ -134,6 +142,12 @@ class AppointmentController {
       appointment.canceled_at = new Date();
 
       await appointment.save();
+
+      await Mail.sendMail({
+         to: `${appointment.provider.name}<${appointment.provider.email}>`,
+         subject: 'Agendamento cancelado',
+         text: 'Você tem um novo cancelamento',
+      });
 
       return res.json(appointment);
    }
